c7709c0369923a3ebcac0c8d648065ef4bb84a89
Kartik Narang <kartik.narang@borngroup.com>
2024-11-28 23:01:45 +0530

    second commit

---
+++ pages/api/contactpostblogs.js
@@ -0,0 +1,21 @@
+  import { NextApiRequest, NextApiResponse } from 'next';
+  import fs from 'fs';
+  import path from 'path';
+
+  export default async function handler(req, res) {
+      if (req.method === 'POST') {
+          // Handle the POST request
+          const data = req.body;
+          const { name, lastname, phone, email } = data;
+          console.log(name);
+
+          // Save the data to a file
+          const filePath = path.join(process.cwd(), 'data.json');
+          fs.appendFileSync(filePath, JSON.stringify(data) + '\n');
+
+          res.status(200).json({ message: 'Data received successfully', data });
+      } else {
+          // Handle any other HTTP method
+          res.status(200).json({ msg: 'Get method detected request rejected' });
+      }
+  }
